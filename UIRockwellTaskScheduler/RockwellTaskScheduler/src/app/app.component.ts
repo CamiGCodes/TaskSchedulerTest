65f85a80d65465fdc6921caa3587c27c1718f28e
Maria Camila Chica Gomez <camilacode@proton.me>
2024-04-12 00:18:04 -0500

    Adding styles, color border, scrolling

--- UIRockwellTaskScheduler/RockwellTaskScheduler/src/app/app.component.ts
+++ UIRockwellTaskScheduler/RockwellTaskScheduler/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HangfireService } from './hangfire.service';
 
